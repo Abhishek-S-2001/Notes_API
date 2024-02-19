55db38582cda3be6dc2ad72a263344d1c068da73
Abhishek Shekhawat <85601866+Abhishek-S-2001@users.noreply.github.com>
2024-02-19 18:57:40 +0530

    Created Databases

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const body_parser = require('body-parser');
+
+const mongo_db = require('./mongo_db');
+const connect_notesdb = require('./mongo_db');
+
 const app = express();
+
+// Middleware to parse JSON in the request body
+app.use(body_parser.json());
 
 app.get("/", (req, res) => {
     return res.send("API Started on port 3000");
 });
 
-mongoose.connect("mongodb://localhost:27017/sample_db")
-        .then(() => console.log("MongoDB Server Connected"))
-        .catch((err) => console.log("Mongo ERROR", err));
-
-app.get
+note = connect_notesdb();
 
 app.listen(3000, () => {
   console.log('Listening on port http://localhost:3000/')
